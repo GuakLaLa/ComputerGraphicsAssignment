c171f84e17cef7a7591571f9144fb8b00f789105
GuakLaLa <zhiwei.tan2004@gmail.com>
2025-06-03 22:34:02 +0800

    create mercury

--- src/computergraphicsassignment/component/Mercury.java
+++ src/computergraphicsassignment/component/Mercury.java
@@ -4,10 +4,40 @@
  */
 package computergraphicsassignment.component;
 
-/**
- *
- * @author Tan Zhi Wei
- */
+
+import com.jogamp.opengl.GL2;
+import com.jogamp.opengl.glu.GLU;
+import com.jogamp.opengl.glu.GLUquadric;
+
 public class Mercury {
-    
+    private float radius;
+    private float distanceFromSun;
+    private float[] color;
+
+    public Mercury(float radius, float distanceFromSun, float[] color) {
+        this.radius = radius;
+        this.distanceFromSun = distanceFromSun;
+        this.color = color;
+    }
+
+    public void draw(GL2 gl, GLU glu, float angle) {
+        gl.glPushMatrix();
+
+        // Revolve around the Sun
+        gl.glRotatef(angle * 2f, 0f, 1f, 0f);
+        gl.glTranslatef(distanceFromSun, 0f, 0f);
+
+        // Rotate on its own axis
+        gl.glRotatef(angle * 5f, 0f, 1f, 0f);
+
+        // Set color
+        gl.glColor3f(color[0], color[1], color[2]);
+
+        // Draw sphere
+        GLUquadric quad = glu.gluNewQuadric();
+        glu.gluSphere(quad, radius, 30, 30);
+        glu.gluDeleteQuadric(quad);
+
+        gl.glPopMatrix();
+    }
 }
